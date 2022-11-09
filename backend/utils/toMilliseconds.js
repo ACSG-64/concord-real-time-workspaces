d3a63330b18e4d3d766f15b0a004d953b70fe919
Andrés Sarmiento <acamilo707@gmail.com>
2022-11-09 18:31:29 -0500

    Added a util to convert different time units to milliseconds

---
+++ backend/utils/toMilliseconds.js
@@ -0,0 +1,18 @@
+/**
+ * Converts a time unit to milliseconds.
+ * @param {{
+ * days: number, 
+ * hours: number, 
+ * minutes: number, 
+ * seconds: number }} baseTime - A time unit to convert to milliseconds. Only one unit can be chosen
+ * @returns The input time in milliseconds
+ */
+function toMilliseconds({ days, hours, minutes, seconds }) {
+	if (seconds) return seconds * 1000;
+	else if (minutes) return minutes * 60 * 1000;
+	else if (hours) return hours * 60 * 60 * 1000;
+	else if (days) return days * 24 * 60 * 60 * 100;	
+	throw 'Invalid input';
+}
+
+module.exports = toMilliseconds;
