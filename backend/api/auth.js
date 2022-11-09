7c3571eeed52e3d877bfd6f0df177d07de003b28
Andrés Sarmiento <acamilo707@gmail.com>
2022-11-09 18:33:51 -0500

    Added the functionality to login using email and password following the initial specifications of issue #10

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { register } = require('../controllers/auth/index');
+const { register, login } = require('../controllers/auth/index');
 
 const router = express.Router();
 
 router.post('/register', register.validators, register.controller);
+router.post('/login', login.validators, login.controller);
 
 module.exports = router;
