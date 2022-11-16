17d08935ee09335eced864ac28885e57e2f37aab
Andrés Sarmiento <acamilo707@gmail.com>
2022-11-16 16:50:59 -0500

    Added a global middleware to compress responses

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,6 @@ const args = require('./utils/args');
 const express = require('express');
 const { createServer } = require('http');
 const path = require('path');
-const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
 
 const { authEndpoint } = require('./api/index');
 
@@ -16,10 +14,12 @@ const server = createServer(app);
 app.use(express.static(path.join(__dirname, 'public')));
 
 /* Configure global middlewares */
-app.use(helmet()); // set HTTP security headers
+app.use(require('helmet')()); // set HTTP security headers
+app.use(require('compression')()); // compress all the responses to reduce bandwidth usage
+// Parsers
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(cookieParser());
+app.use(require('cookie-parser')());
 
 /* Set the API endpoints */
 app.use('/api/auth', authEndpoint);
