90e7885bd7cb08e7faaf4bde9628927b51d93be4
Andrés Sarmiento <acamilo707@gmail.com>
2022-11-16 20:30:55 -0500

    Now, if the NODE_ENV variable is set to 'production' , the argument is also set to production

--- backend/utils/args.js
+++ backend/utils/args.js
@@ -5,7 +5,7 @@ const { hideBin } = require('yargs/helpers');
 /**
  * Access parameters passed from the command line
  */
-
 const args = yargs(hideBin(process.argv)).argv;
+// If the environment variable is set to 'production', override the argument 'mode'
 if (process.env.NODE_ENV === 'production') args.mode = 'production';
 module.exports = args;
