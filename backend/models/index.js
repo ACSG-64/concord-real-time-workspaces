75eebe2eb6ed7206488cac64fd8f5deceb497e1c
Nathan Norton <nathangnorton@gmail.com>
2022-11-18 16:50:15 +0100

    added workspaces model to models issue #12

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,9 +1,11 @@
 const User = require('./user');
 const Message = require('./message');
 const Channel = require('./channel');
+const Workspace = require('./workspace');
 
 module.exports = {
     User,
     Message,
     Channel,
+    Workspace,
 };
