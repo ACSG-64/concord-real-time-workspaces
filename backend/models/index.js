592cca00b293f77f6fbdbd759a9b78d9475da902
Nathan Norton <nathangnorton@gmail.com>
2022-11-18 16:57:26 +0100

    added membership model to models issue #12

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,10 +2,12 @@ const User = require('./user');
 const Message = require('./message');
 const Channel = require('./channel');
 const Workspace = require('./workspace');
+const Membership = require('./membership');
 
 module.exports = {
     User,
     Message,
     Channel,
     Workspace,
+    membership,
 };
