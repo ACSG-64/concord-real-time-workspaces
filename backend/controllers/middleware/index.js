b23ebd98086a3c2cba7aa02ae6fe0f48eb456352
Andrés Sarmiento <acamilo707@gmail.com>
2022-11-24 22:59:40 -0500

    Developed the client authorization middleware. Fixed issue #13

--- backend/controllers/middleware/index.js
+++ backend/controllers/middleware/index.js
@@ -1,5 +1,7 @@
+const clientAuthorization = require('./client-authorization');
 const validationErrorHandler = require('./validation-error-handler');
 
 module.exports = {
+    clientAuthorization,
     validationErrorHandler,
 };
