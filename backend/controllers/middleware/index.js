7c1c5dcef5348d40eeba2c0b28c86df3988d9921
Andrés Sarmiento <acamilo707@gmail.com>
2022-11-26 22:04:46 -0500

    A lightweight authorization middleware was added. It only verifies the validity of the authorization cookie

--- backend/controllers/middleware/index.js
+++ backend/controllers/middleware/index.js
@@ -1,7 +1,9 @@
 const clientAuthorization = require('./client-authorization');
+const weakClientAuthorization = require('./weak-client-authorization');
 const validationErrorHandler = require('./validation-error-handler');
 
 module.exports = {
     clientAuthorization,
+    weakClientAuthorization,
     validationErrorHandler,
 };
---
+++ backend/controllers/middleware/weak-client-authorization.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+/**
+ * Authorization middleware. Verifies the validity of the JWT provided by the client through the auth cookie, 
+ * if it is invalid, prevents to go to the next handler. 
+ * @param {Express.req} req 
+ * @param {Express.res} res 
+ * @param {Express.next} next 
+ */
+module.exports = async function (req, res, next) {
+    try {
+        // Verify the JWT from the cookie
+        await jwt.verify(req.cookies['concord_auth'], process.env.JWT_SECRET);        
+    } catch (e) {
+        // If the JWT is invalid, respond immediately
+        res.status(401).send('You need to authenticate first');
+    }
+
+    next(); // if the JWT is valid, proceed to the next handler
+};
