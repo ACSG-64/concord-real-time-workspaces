8f53a20f77c47e99618adaace421e8710341feca
Joshua Ollivierre <joshino784@gmail.com>
2022-11-28 11:23:05 -0400

    Added base relationships
    Added base relationships to index, awaiting how the relationships really work so I can finish

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,6 +3,42 @@ const Message = require('./message');
 const Channel = require('./channel');
 const Workspace = require('./workspace');
 const Membership = require('./membership');
+const { DataTypes } = require('sequelize');
+
+
+User.hasMany(Message, {
+    foreignKey: {
+        allowNull: false,
+        type: DataTypes.UUID,
+    },
+    onDelete: 'CASCADE',
+});
+Message.belongsTo(User);
+
+User.hasMany(Membership, {
+    onDelete: 'CASCADE'
+});
+Membership.belongsTo(User);
+
+Workspace.hasMany(Membership, {
+    onDelete: 'CASCADE'
+});
+Membership.belongsTo(Workspace);
+
+Workspace.hasMany(Channel, {
+    onDelete: 'CASCADE'
+});
+Channel.belongsTo(Workspace);
+
+Channel.hasMany(Message, {
+    onDelete: 'CASCADE'
+});
+Message.belongsTo(Channel);
+
+Message.hasMany(Message, {
+    onDelete: 'CASCADE'
+});
+Message.belongsTo(Message);
 
 module.exports = {
     User,
