abcb65ae3386146cbfec7dc7aa725785c5a32b30
Joshua Ollivierre <Joshino784@gmail.com>
2022-11-29 08:03:18 -0400

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -5,32 +5,39 @@ const Workspace = require('./workspace');
 const Membership = require('./membership');
 
 
-//Set Associations
+//ASSOCIATIONS
+
+//User-Message
 User.hasMany(Message);
 Message.belongsTo(User);
 
+//User-Workspace
 User.belongsToMany(Workspace, {
     through: Membership
 });
 
+//Workspace-User
 Workspace.belongsToMany(User, {
     through: Membership
 });
 
+//Workspace-Channel
 Workspace.hasMany(Channel, {
     onDelete: 'CASCADE'
 });
 Channel.belongsTo(Workspace);
 
+//Channel-Message
 Channel.hasMany(Message, {
     onDelete: 'CASCADE'
 });
 Message.belongsTo(Channel);
 
+//Message-Message
 Message.hasMany(Message, {
-    onDelete: 'CASCADE'
+    onDelete: 'CASCADE',
+    foreignKey: 'replyToMsgId'
 });
-Message.belongsTo(Message);
 
 module.exports = {
     User,
