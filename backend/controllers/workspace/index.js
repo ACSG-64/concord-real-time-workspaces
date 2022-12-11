c32eb77326a97b432449d044e2e10a0c3c8164fb
Andrés Sarmiento <acamilo707@gmail.com>
2022-12-11 01:14:11 -0500

    Exports were expanded and reorganized

--- backend/controllers/workspace/index.js
+++ backend/controllers/workspace/index.js
@@ -1,9 +1,16 @@
-const create = require('./create-workspace');
-const remove = require('./delete-workspace');
+/* General controllers */
+const joinWorkspace = require('./general/join-workspace');
 
-
+/* Management controllers */
+const createWorkspace = require('./management/create-workspace');
+const removeWorkspace = require('./management/delete-workspace');
+const generateInvitation = require('./management/generate-invitation');
+const toggleInvitation = require('./management/toggle-invitation');
 
 module.exports = {
-    create,
-    remove
+    general: { joinWorkspace },
+    management: {
+        createWorkspace, removeWorkspace,
+        generateInvitation, toggleInvitation,
+    },
 };
